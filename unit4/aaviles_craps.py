cbc9411f8b23559f2aa16850be0a68703c76b77f
Antonio <antonioaviles@uagateway.org>
2016-12-16 17:11:14 +0000

    work on genrating random number

--- unit4/aaviles_craps.py
+++ unit4/aaviles_craps.py
@@ -4,8 +4,10 @@ import random
 def roll2dice():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
-    return("The roll is {}+{}".format(dice1,dice2))
-    
+    dice_sum = dice1 + dice2
+    print("Roll was {} and {}. Equal to {}".format(dice1, dice2, dice_sum))
+    return dice_sum
+
 #function name: bank 
 #agurment: none
 #purpose :how much money the player has.
@@ -20,27 +22,19 @@ def get_player_bank():
 def get_bet(bank):
     bet = (int(input("Place your bet: ")))
     if bet <= bank:
-        return bet
+        return("Your bet was {}".format(bet))
     elif bet != bank:
         print(int(input("You don't have that amount place another bet: ")))
-get_bet(100)
-        
+
 #function name: get_first_phase
-#argument: point_number
+#argument:
 #purpose: to set the wining numbers
 #returns: the wining number for phase one.
-def get_first_phase():
+def get_first_phase(dice_sum):
     if dice_sum == 2 or dice_sum == 7:
         return("Player wins")
     elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
-        return("Computer wins")
-#OPTION 2
-# function name: roll_result
-#   purpose: get the result of the roll
-#   arguments: 
-#       roll - the sum of the two dice rolled
-#       is_first_roll - true/false depending on which roll it is
-#   returns: the result
-#       if player wins: return "win"
-#       if player lost: return "lose"
-
+        print("Computer wins")
+    else:
+        return("Reroll")
+get_first_phase(2)
