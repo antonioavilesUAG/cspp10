bc5deae97cb86561032da3ba31660e178fece374
Antonio <antonioaviles@uagateway.org>
2017-02-03 17:16:29 +0000

    Today i started working on the scramble the coding

--- unit5/AP_project.py
+++ unit5/AP_project.py
@@ -1,12 +1,14 @@
-username = input("Set a username: ")
-password = (int(input("Set a password that is numbers only: ")))
+#username = input("Set a username: ")#
+#password = (int(input("Set a password that is numbers only: ")))#
 
-print("you must verify your self")
+#print("you must verify your self")#
 
-username2 = input("Put in your username: ")
-password2 = (int(input("Put in your password: "))) 
-if username == username2 and password == password2:
-    print("You may enter your email")
-else:
-    print("This email is not yours")
-       
+#username2 = input("Put in your username: ")#
+#password2 = (int(input("Put in your password: ")))# 
+#if username == username2 and password == password2:#
+    #print("You may enter your email")#
+#else:
+    #print("This email is not yours")#
+def get_guess: 
+    
+    
---
+++ unit5/aviles_scramble.py
@@ -0,0 +1,12 @@
+import random
+
+def scramble_word():
+#Allow user to put in there word that they want to scramble#
+    player1 = input("Write in a word: ")
+#Get the first and last letter by thereselves by using string indexs do that#
+    firstletter = player1[1:-1]
+    print(firstletter)
+#Put the rest of the word into a list.# 
+#Scumble the list# 
+#combine the first and last letter with the words you scrumble# 
+scramble_word()
