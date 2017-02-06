8b4e6ca5c0783abb8299e0a2629f39cf41a48143
Antonio <antonioaviles@uagateway.org>
2017-02-06 17:15:03 +0000

    Work on the scramble code. work on combine the list and word1 and 2

--- unit5/AP_project.py
+++ unit5/AP_project.py
@@ -1,3 +1,4 @@
+import random
 #username = input("Set a username: ")#
 #password = (int(input("Set a password that is numbers only: ")))#
 
@@ -9,6 +10,48 @@
     #print("You may enter your email")#
 #else:
     #print("This email is not yours")#
-def get_guess: 
-    
-    
+def get_bet(bank):
+    bet = (int(input("How much you want to bet: ")))
+    if bet <= bank:
+        return(bet)
+    elif bet != bank:
+        return(int(input("You dont have that. Place new bet: ")))
+def get_guess(): 
+    player1 = (int(input("What cap is the bottle in out of the 3: ")))
+    if player1 == 1:
+        return 1
+    elif player1 == 2:
+        return 2 
+    elif player1 == 3:
+        return 3 
+def get_comp_move():
+    dealer = random.randint(1,4)
+    if dealer == 1:
+        return 1
+    elif dealer == 2:
+        return 2
+    elif dealer == 3:
+        return 3 
+    elif dealer == 4:
+        return 4 
+def get_winner(player1, dealer):
+    if player1 == dealer:
+        return("player1") 
+    elif player1 != dealer:
+        return ("dealer") 
+def fordham_game():
+    bank = 100 
+    while bank != 0:
+        bet = get_bet(bank) 
+        player1 = get_guess()
+        dealer = get_comp_move() 
+        winner = get_winner(player1, dealer)
+        if winner == "player1":
+            print("Player wins")
+            bank = bank + bet * 2 
+            print(bank) 
+        elif winner == "dealer":
+            print("Dealer wins")
+            bank = bank - bet
+            print(bank)
+fordham_game()
--- unit5/aviles_scramble.py
+++ unit5/aviles_scramble.py
@@ -4,9 +4,16 @@ def scramble_word():
 #Allow user to put in there word that they want to scramble#
     player1 = input("Write in a word: ")
 #Get the first and last letter by thereselves by using string indexs do that#
-    firstletter = player1[1:-1]
-    print(firstletter)
+    word = player1[0]
+    word2 = player1[-1]
+    firstlastletter = player1[1:-1]
+    print(firstlastletter)
 #Put the rest of the word into a list.# 
+    a = list(firstlastletter)
+    print(a)
 #Scumble the list# 
+    random.shuffle(a)
+    print(a)
 #combine the first and last letter with the words you scrumble# 
+    word3 = word + a + word2
 scramble_word()
