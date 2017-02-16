9ce868a608534456087bd5de8d0bf7a9987a01d0
Antonio <antonioaviles@uagateway.org>
2017-02-16 17:16:02 +0000

    work on the updata function.Finish the rest of the function.

--- unit5/dictionarya.py
+++ unit5/dictionarya.py
@@ -7,4 +7,11 @@ def get_add(d):
     key = input("Input key: ")
     value = input("Input value: ")
     d[key] = value
-print(get_add(d))
+get_add(d)
+pprint(d)
+
+def get_remove_key(d):
+    remove = input("What do you want to delete: ")
+    del d[remove]
+get_remove_key(d)
+pprint(d)
