83b6349939be256595c25c1461bb5a3bce93eb2a
Antonio <antonioaviles@uagateway.org>
2017-02-17 17:15:44 +0000

    work on the main game next. All other function work.

--- unit5/dictionarya.py
+++ unit5/dictionarya.py
@@ -15,3 +15,11 @@ def get_remove_key(d):
     del d[remove]
 get_remove_key(d)
 pprint(d)
+def get_update(d):
+    update_key1 = input("What key do you want to update: ")
+    update_value2 = input("What value do you want to update: ")
+    d[update_key1] = update_value2
+get_update(d)
+pprint(d)
+def exit(d):
+    print(d)
